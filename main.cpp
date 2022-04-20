b7093c5ced5bd82f8ed679d52af8bc0429839d3b
frooia <frooia77@gmail.com>
2022-04-20 18:09:40 -0400

    Tried to merge everything together

--- main.cpp
+++ main.cpp
@@ -6,6 +6,19 @@
 #include "Buttons.h"
 #include "SelectionMenu.h"
 #include "GamePlay.h"
+#include "Set.cpp"
+#include <fstream>
+
+vector<string> readWordList(int wordLength) {
+    ifstream file("word lists/" + to_string(wordLength) + "-letter-words.txt");
+    vector<string> ans;
+    while (file) {
+        string word;
+        file >> word;
+        ans.push_back(word);
+    }
+    return ans;
+}
 
 int main()
 {
@@ -22,16 +35,23 @@ int main()
             
             if (event.type == sf::Event::MouseButtonPressed) {
                 sf::Vector2f mousePosition(sf::Mouse::getPosition(window).x, sf::Mouse::getPosition(window).y);
-
+                Set set(5);
+                vector<string> wordList;
                 if (game.MenuTime()) {
                     game.ChooseWordLength(mousePosition);  
-                    //can get wordlength here for guess generation 
-                    //int len = game.WordLength()
-                    game.FinalizeWordLength(mousePosition, "LAUREN"); //FIRST GUESS STRING GOES HERE
+                    // Get wordLength here for guess generation
+                    int len = game.WordLength();
+                    wordList = readWordList(len);
+                    set.length = len;
+                    set.generate(wordList);
+                    game.FinalizeWordLength(mousePosition, set.randomGuess()); //FIRST GUESS STRING GOES HERE
                 }
                 else {
                     vector<pair<char, int>> feedback;
-                    game.Play(mousePosition, "LAUREN", feedback); //SUBSEQUENT GUESSES GO HERE
+                    // game.Play(mousePosition, "LAUREN", feedback); //SUBSEQUENT GUESSES GO HERE
+                    game.GetFeedBack(mousePosition, feedback);
+                    set.update(feedback);
+                    game.SetNextGuess(set.randomGuess());
                 }
             }
         }
